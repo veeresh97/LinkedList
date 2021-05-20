5c7a5287eb8f54dbf49699f17cda230fea883a2a
veeresh97 <67183089+veeresh97@users.noreply.github.com>
2021-05-20 23:26:45 +0530

    Added: simple link list

--- main/java/com/DSLinkList/LinkedList.java
+++
@@ -1,7 +0,0 @@
-package com.DSLinkList;
-
-public class LinkedList {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Link List");
-    }
-}
---
+++ test/java/com/DSLinkList/MyNodeTest.java
@@ -0,0 +1,18 @@
+package com.DSLinkList;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyNodeTest {
+    @Test
+    public void given3NumberWhenLinkedShouldPassedLinkedListTest(){
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                mySecondNode.getNext().equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
+}
